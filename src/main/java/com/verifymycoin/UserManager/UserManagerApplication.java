24e4343454a5944ad0a069c7dabd05293465a3b3
sophysophysophysophy <61459@naver.com>
2021-09-14 21:53:09 +0900

    redis session 초기 구성

--- src/main/java/com/verifymycoin/UserManager/UserManagerApplication.java
+++ src/main/java/com/verifymycoin/UserManager/UserManagerApplication.java
@@ -1,25 +1,32 @@
 package com.verifymycoin.UserManager;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class UserManagerApplication {
+@EnableRedisHttpSession
+public class UserManagerApplication implements CommandLineRunner {
 
-	@Value("${foo}")
-	private String configExample;
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello " + configExample;
-	}
+//	@Value("${foo}")
+//	private String configExample;
+//R
+//	@RequestMapping("/")
+//	public String home() {
+//		return "Hello " + configExample;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("redis session test ");
+
+	}
 }
