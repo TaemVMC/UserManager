d902950eb16c5917c3677b599e1b95ac10fef98a
taehoonhwang <on91716@gmail.com>
2021-09-14 16:24:05 +0900

    feat: 도커라이징 및 환경별 config 분리
    도커라이징
    - local 스테이지 : 별도 빌드 과정 미포함. Docker build에 앞서 java 빌드 필요(.jar파일 필요). application-local.yml 적용됨
    - development 스테이지 : 빌드 과정 포함. git repo - development 브랜치를 기반으로 컨테이너 내에서 빌드.  application-dev.yml 적용됨

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM openjdk:11 AS local
+WORKDIR /app
+COPY build/libs/*.jar ./app.jar
+CMD ["java", "-Dspring.profiles.active=local", "-jar", "app.jar"]
+
+
+FROM openjdk:11 AS builder
+WORKDIR /app
+COPY . /app
+RUN ./gradlew clean build
+COPY build/libs/*.jar /app.jar
+
+FROM builder AS development
+WORKDIR /
+CMD ["java", "-Dspring.profiles.active=dev", "-jar", "app.jar"]
--- src/main/java/com/verifymycoin/UserManager/UserManagerApplication.java
+++ src/main/java/com/verifymycoin/UserManager/UserManagerApplication.java
@@ -1,10 +1,22 @@
 package com.verifymycoin.UserManager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserManagerApplication {
+
+	@Value("${foo}")
+	private String configExample;
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello " + configExample;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagerApplication.class, args);
